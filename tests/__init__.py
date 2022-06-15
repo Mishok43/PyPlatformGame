723c3c0ee49b9ad423bedc5c1905868d5763d111
mishok43 <devmishok@gmail.com>
2022-06-16 00:32:36 +0300

    Fixed in test __init__.py

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,4 +1,4 @@
 from .aabb_test import AABBTest
 from .audio_test import AudioManagerTest
-from .gravity_test import AABBTest
+from .gravity_test import GravityTest
 from .disjointed_hurtbox_test import DisjointedHurtboxTest
